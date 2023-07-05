993b3c475d4db226e5cfc2c9f7d171445c55567c
kevin-magu <wanjaukevinmagu@gmail.com>
2023-07-05 11:12:05 +0300

    u

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { GitHub } from '@mui/icons-material';
+import { Twitter } from '@mui/icons-material';
+import { LinkedIn } from '@mui/icons-material';
+import { WhatsApp } from '@mui/icons-material';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='app-title'><p>Taskify</p></div>
+      <footer>
+        
+      </footer>
     </div>
   );
 }
