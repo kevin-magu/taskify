953553bb29ffc3330b6eba4678f685324b95a15d
kevin-magu <wanjaukevinmagu@gmail.com>
2023-07-05 21:52:02 +0300

    U

--- src/components/home-page/Leftside.js
+++ src/components/home-page/Leftside.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { EmojiEmotions } from "@mui/icons-material";
 
 function Leftside() {
   return (
-    <div className='left-side'>
+    <div className="left-side">
       <p>
         Welcome to our website! We're thrilled to have you here. Explore our
         platform and discover the tools and resources that will help you achieve
         your goals. Get ready to boost your productivity and stay organized with
         our innovative task management solution.
       </p>
-      <p className='slogan'>Stay on top of your game with Taskify's powerful task management</p>
+      <p className="slogan">
+        Unleash your productivity potential and conquer your goals effortlessly.
+        <EmojiEmotions className="face"/>
+      </p>
+
+      <Link to="/createtasks" className="get-started">
+        {" "}
+        <button>Get Started</button>{" "}
+      </Link>
     </div>
-  )
+  );
 }
 
-export default Leftside
+export default Leftside;
