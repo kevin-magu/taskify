894642f7457ac2f9cedae7182d5d70445720c7f7
kevin-magu <wanjaukevinmagu@gmail.com>
2023-07-05 13:55:25 +0300

    u

---
+++ src/components/main-page/Taskcard.js
@@ -0,0 +1,16 @@
+import { Toolbar } from "@mui/material"
+
+function Taskcard() {
+  return (
+    <div className='task-card'>
+        <p className='task-description'>
+            Finish Introduction to computer Science -Havard
+        </p>
+        <p className='task-status'>Status: <span>Pending...</span></p>
+        <p className='task-due-date'>Due Date: 12/07/2023</p>
+        <Toolbar /> 
+    </div>
+  )
+}
+
+export default Taskcard
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import Createtask from "../components/main-page/Createtask";
 import Userpic from "../components/main-page/Userpic";
-import Loginbutton from "../components/main-page/Loginbutton";
+import Loginbutton from "../components/main-page/Loginbutton"; 
+import Taskcard from "../components/main-page/Taskcard";
 
 import '../styles/Home.css';
 function Home() {
@@ -10,10 +11,11 @@ function Home() {
           <Createtask />
           <Userpic />
           <Loginbutton />
-          <div className="task-card-container">
-
-          </div>
         </div>
+        <div className="task-card-container">
+              <Taskcard />
+              <Taskcard />
+          </div>
     </div>
   )
 }
