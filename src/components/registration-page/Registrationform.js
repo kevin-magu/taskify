96f57bd3fd1a8be8cce5addee9c836e3ba858495
kevin-magu <wanjaukevinmagu@gmail.com>
2023-07-07 12:55:23 +0300

    U

--- src/components/registration-page/Registrationform.js
+++ src/components/registration-page/Registrationform.js
@@ -2,24 +2,21 @@ import Form from "../../components/login-page/Form";
 import '../../styles/Login.css'
 import { Link } from "react-router-dom";
 import { Google } from "@mui/icons-material";
-import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
-
-
-import Slide from '@mui/material/Slide';
+import { Snackbar } from "@mui/material";
 import { useState } from "react";
 //firebase ini
 
 import { auth } from "../../Firebaseconfig";
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, createUserWithEmailAndPassword, AuthErrorCodes } from 'firebase/auth';
 
 function Registrationform() {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-  const [snackbarOpen, setSnackbarOpen] = useState(false)
-  const auth = getAuth()
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const auth = getAuth();
   const handleRegister = async (e) =>{
     e.preventDefault();
 
@@ -31,7 +28,11 @@ function Registrationform() {
       await createUserWithEmailAndPassword(auth, email, password);
       console.log("successiful");
     } catch (error) {
-      setErrorMessage(error.message);
+      if (error.code === AuthErrorCodes.EMAIL_EXISTS) {
+        setErrorMessage('Email address is already in use')
+      }else{
+        setErrorMessage(error.message);
+      }
       setSnackbarOpen(true);
     } 
 };
@@ -49,12 +50,14 @@ const handleSnackbarClose = () => {
       
       </div>
       <div className='form-right-side'>
+      
+
         <form onSubmit={handleRegister}>
           <label>Email</label>
-          <input type="text" placeholder='Enter your email' value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="email" placeholder='Enter your email' value={email} onChange={(e) => setEmail(e.target.value)} required/>
 
           <label id='label'>Password</label>
-          <input type="text" placeholder='Enter your Password' onChange={(e) => setPassword(e.target.value)}/>
+          <input type="text" placeholder='Enter your Password' onChange={(e) => setPassword(e.target.value)} required/>
           <button type="submit">Register</button>
 
           <p className='sign-in-with-google'> 
@@ -64,11 +67,12 @@ const handleSnackbarClose = () => {
            <Link to="/login" className='reginster-link'><button>Login here</button> </Link> </p> 
         </form>
 
-        <Snackbar open={snackbarOpen} autoHideDuration={2000} onClose={handleSnackbarClose}>
+        <Snackbar open={snackbarOpen} autoHideDuration={5000} onClose={handleSnackbarClose}>
         <MuiAlert onClose={handleSnackbarClose} severity="error" variant="filled">
         {errorMessage}
         </MuiAlert>
-        </Snackbar>
+      </Snackbar>
+        
       </div>
     </div>
   )
