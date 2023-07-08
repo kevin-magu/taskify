8ae2acf305da5561119f7f79de1fe4ebfee48268
kevin-magu <wanjaukevinmagu@gmail.com>
2023-07-08 22:10:47 +0300

    U

--- src/components/createtaskmain-page/Taskform.js
+++ src/components/createtaskmain-page/Taskform.js
@@ -1,12 +1,15 @@
 import { initializeApp } from "firebase/app"
-import {getFirestore} from "firebase/firestore"
+import {getFirestore} from "@firebase/firestore"
 import {app} from "../../Firebaseconfig"
 import { useState } from "react";
+
+
 
 function Taskform() {
     const db = getFirestore(app);
     const [taskTitle, setTaskTitle] = useState('');
     const [taskDescription, setTaskDescription] = useState('');
+    const [dueDate, setDueDate] = useState('');
     const [taskPriority, setTaskPriority] = useState('');
     const [taskStatus, setTaskStatus] = useState('In Progress')
 
@@ -16,6 +19,7 @@ function Taskform() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
     }
 
   return (
@@ -26,32 +30,32 @@ function Taskform() {
     <form className="create-task-form">
 
       <label>Task Title</label>
-      <input type="text" className='input-fields'/>
+      <input type="text" value={taskTitle} className='input-fields' onChange={(e) => setTaskTitle(e.target.value)}/>
 
       <label> Task Descscription</label>
-      <textarea cols="30" rows="10"></textarea> 
+      <textarea cols="30" rows="10" value={taskDescription} onChange={(e)=> setTaskDescription(e.target.value)}></textarea> 
 
       <label>Due Date</label>
-      <input type="date" className='input-fields' />
+      <input type="date" className='input-fields' value={dueDate} onChange={(e) => setDueDate(e.target.value)}/>
 
       <label>Task Priority</label>
       <div  className='priority-labels'>
         <label className='priority-label'>High</label>
-        <input type="checkbox" />
+        <input type="checkbox" checked={highPriority} onChange={(e)=> setHighPriority(e.target.checked)}/>
       </div>
 
       <div>
         <label className='priority-label'>Medium</label>
-        <input type="checkbox" />
+        <input type="checkbox" checked={mediumPriority}  onChange={(e)=> setMediumPriority(e.target.checked)}/>
       </div>
       <div>
         <label className='priority-label'>Low</label>
-        <input type="checkbox" />
+        <input type="checkbox" checked={lowPriority} onChange={(e)=> setLowPriority(e.target.checked)} />
       </div>
       <label>Status</label>
         <div>
-        <select>
-            <option value="" key=""></option>
+        <select value={taskStatus} onChange={(e)=> setTaskStatus(e.target.value)}>
+            <option value="In progress" key="">In progress</option>
             <option value="status" key="">Pending</option>
             <option value="status" key="">Complete</option>
         </select>
