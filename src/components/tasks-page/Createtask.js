b8a4a254c56d64313fc732156f8c3195b1a9232e
kevin-magu <wanjaukevinmagu@gmail.com>
2023-07-08 03:45:20 +0300

    U

--- src/components/tasks-page/Createtask.js
+++ src/components/tasks-page/Createtask.js
@@ -1,9 +1,11 @@
-
+import { Link } from "react-router-dom"
 
 function Createtask() {
   return (
     <div>
+        <Link id="link-to-edit" to={'/Edit'}>
         <button className="create-task-button"> <span>+</span> <p>Create Task</p></button>
+        </Link>
     </div>
   )
 }
